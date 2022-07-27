eb3e9e6a49896c4c2149c5809ad120b640247a1c
Владимир Ворона <vvorona665@gmail.com>
2022-07-27 23:06:25 +0300

    End of writing code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] Massiv1 = new string [4] {"1234", "1567", "-2", "computer science"};
+string [] Massiv2 = new string [Massiv1.Length];
+int count = 0;
+
+void CheckingArray (string [] Massiv1, string [] Massiv2)
+{
+    for (int i = 0; i < Massiv1.Length; i++)
+    {
+        if (Massiv1[i].Length <= 3)
+        {
+            Massiv2[count] = Massiv1[i];
+            count++;
+        }
+    }
+}
+
+void PrintMassiv (string [] Massiv2)
+{
+    for (int i = 0; i < Massiv2.Length; i++)
+    {
+        Console.WriteLine($"{Massiv2[i]} ");
+    }
+}
+
+CheckingArray(Massiv1,Massiv2);
+PrintMassiv(Massiv2);
